e53e7ef83772793ccd26931b1242fea15e7d0ea6
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-05-20 20:48:09 +0000

    Add javascript.js

--- javascript.js
+++ javascript.js
@@ -9,14 +9,92 @@ const tiM = document.querySelector('span.tiM');
 const img = document.querySelector('img');
 const butts = document.querySelector('button');
 
-fetch('https://api.weatherapi.com/v1/current.json?key=d06df2682f2e4645b99195359241405&q=london',
-    {mode: 'cors'})
-    .then(function(response) {
-        //console.log(response.json());
-        return response.json();
-    })
-    .then(function(response) {
-        
+/* fetch('https://api.weatherapi.com/v1/current.json?key=d06df2682f2e4645b99195359241405&q=london',
+{mode: 'cors'})
+.then(function(response) {
+    //console.log(response.json());
+    return response.json();
+})
+.then(function(response) {
+    
+    country.innerText = response.location.country;
+    naMe.innerText = response.location.name;
+    tiM.innerText = response.location.localtime;
+
+    
+    condition.innerText = response.current.condition.text;
+
+    const resIcon = response.current.condition.icon;
+    const te = "https:" + resIcon;
+    img.src = te;
+
+    ce.innerText = response.current.temp_c;
+    //Fa.innerText = response.current.temp_f;
+
+    ce.style.display = 'block';
+
+    butts.addEventListener("click", () => {
+        if (ce.innerText == response.current.temp_c) {
+            ce.innerText = response.current.temp_f
+        } else {
+            ce.innerText = response.current.temp_c;
+        }
+    });
+    
+}).catch(alert); */
+
+const ser = document.getElementById('search');
+
+ser.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+
+       
+        fetch(`https://api.weatherapi.com/v1/current.json?key=d06df2682f2e4645b99195359241405&q=${event.target.value}`,
+        {mode: 'cors'})
+        .then(function(response) {
+            //console.log(response.json());
+            return response.json();
+        })
+        .then(function(response) {
+            
+            country.innerText = response.location.country;
+            naMe.innerText = response.location.name;
+            tiM.innerText = response.location.localtime;
+
+            
+            condition.innerText = response.current.condition.text;
+
+            const resIcon = response.current.condition.icon;
+            const te = "https:" + resIcon;
+            img.src = te;
+
+            ce.innerText = response.current.temp_c;
+
+            butts.addEventListener("click", () => {
+                ce.innerText = response.current.temp_c;
+                console.log(ce.innerText);
+                if (ce.innerText = response.current.temp_c) {
+                    ce.innerText = response.current.temp_f
+                    //console.log(ce.innerText);
+                } else {
+                    ce.innerText = response.current.temp_c;
+                    console.log(ce.innerText);
+                }
+            });
+            
+        }).catch(alert);
+
+    }
+
+});
+
+
+async function weather(){ 
+    try { 
+        const resp = await fetch('https://api.weatherapi.com/v1/current.json?key=d06df2682f2e4645b99195359241405&q=london',
+        {mode: 'cors'})
+        const response = await resp.json();
+            
         country.innerText = response.location.country;
         naMe.innerText = response.location.name;
         tiM.innerText = response.location.localtime;
@@ -40,48 +118,13 @@ fetch('https://api.weatherapi.com/v1/current.json?key=d06df2682f2e4645b991953592
                 ce.innerText = response.current.temp_c;
             }
         });
-        
-    }).catch(alert);
+            
+    } catch(error) {
+        alert(error);
+    }
+}
 
-    const ser = document.getElementById('search');
+weather();
 
-    ser.addEventListener("keyup", (event) => {
-        if (event.keyCode === 13) {
 
-            fetch(`https://api.weatherapi.com/v1/current.json?key=d06df2682f2e4645b99195359241405&q=${event.target.value}`,
-            {mode: 'cors'})
-            .then(function(response) {
-                //console.log(response.json());
-                return response.json();
-            })
-            .then(function(response) {
-                
-                country.innerText = response.location.country;
-                naMe.innerText = response.location.name;
-                tiM.innerText = response.location.localtime;
-
-                
-                condition.innerText = response.current.condition.text;
-
-                const resIcon = response.current.condition.icon;
-                const te = "https:" + resIcon;
-                img.src = te;
-
-                ce.innerText = response.current.temp_c;
-                
-
-                butts.addEventListener("click", () => {
-                    
-                    if (ce.innerText == response.current.temp_c) {
-                        ce.innerText = response.current.temp_f
-                    } else {
-                        ce.innerText = response.current.temp_c;
-                    }
-                });
-                
-            }).catch(alert);
-
-        }
-
-    });
     
